1d22b2ef7c54c224f1698d90bca4a5879bf5498b
Ankit Kokane <ankitkokane90@gmail.com>
2024-01-13 01:59:02 +0530

    Added mobile navbar

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -1,30 +1,45 @@
 import React from 'react'
 import styles from './styles/navbar.module.css'
 import Link from 'next/link'
-import { IoCartOutline } from 'react-icons/io5'
+import { IoCartOutline, IoHomeOutline } from 'react-icons/io5'
 import Image from 'next/image'
+import { CiShoppingTag } from 'react-icons/ci'
 
 export default function Navbar() {
+
     return (
-        <div className={styles.navbar}>
-            <div className={styles.left_elems}>
-                <Link href="/">
-                    Home
-                </Link>
-            <Link href="/">
-                Products
-            </Link>
-            </div >
-            <div className={styles.center_logo}>
-                <Link href="/">
-                    <div className={styles.image}>
-                    <Image src='/logo.png' alt='' fill style={{objectFit:'cover'}}/>
-                    </div>
-                </Link>
+        <div>
+            <div className={styles.navbar}>
+                <div className={styles.left_elems}>
+                    <Link href="/" className={styles.link}>
+                        Home
+                    </Link>
+                    <Link href="/" className={styles.link}>
+                        Products
+                    </Link>
+                </div >
+                <div className={styles.center_logo}>
+                    <Link href="/">
+                        <div className={styles.image}>
+                            <Image src='/logo.png' alt='' fill style={{ objectFit: 'cover' }} />
+                        </div>
+                    </Link>
+                </div>
+                <div className={styles.right_elems}>
+                    <Link href="/" className={styles.link}>
+                        <IoCartOutline size={25} />
+                    </Link>
+                </div>
             </div>
-            <div className={styles.right_elems}>
-                <Link href="/">
-                    <IoCartOutline size={25}/>
+            <div className={styles.navbar_mobile}>
+                <Link href="/" className={styles.link}>
+                    <CiShoppingTag size={25} />
+                </Link>                
+                <Link href="/" className={styles.link}>
+                    <IoHomeOutline size={25} />
+                </Link>
+                <Link href="/" className={styles.link}>
+                    <IoCartOutline size={25} />
                 </Link>
             </div>
         </div>
