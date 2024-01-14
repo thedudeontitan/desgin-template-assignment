f33ad69e68e712cade37cf091a022c1458966155
Ankit Kokane <ankitkokane90@gmail.com>
2024-01-15 03:14:29 +0530

    UI changes for responsiveness

--- app/_components/Product.tsx
+++ app/_components/Product.tsx
@@ -18,7 +18,7 @@ export default function Product({ product }: { product: Product }) {
     if (modalVisible) {
       const timer = setTimeout(() => {
         setModalVisible(false);
-      }, 3000);
+      }, 300000);
       return () => clearTimeout(timer);
     }
   }, [modalVisible]);
@@ -26,23 +26,25 @@ export default function Product({ product }: { product: Product }) {
   return (
     <div className={styles.product}>
       <Link href={`/products/${product.id}`} style={{ textDecoration: "none" }}>
-        <div className={styles.image_container}>
-          <div className={styles.image}>
-            <Image
-              src={product.image}
-              alt=""
-              fill
-              unoptimized
-              style={{ objectFit: "contain" }}
-            />
+        <div className={styles.image_details_wrapper}>
+          <div className={styles.image_container}>
+            <div className={styles.image}>
+              <Image
+                src={product.image}
+                alt=""
+                fill
+                unoptimized
+                style={{ objectFit: "contain" }}
+              />
+            </div>
           </div>
-        </div>
-        <div className={styles.details_container}>
-          <div className={styles.title_wrapper}>
-            <p className={styles.title}>{product.title}</p>
-            <p className={styles.price}>${product.price}</p>
+          <div className={styles.details_container}>
+            <div className={styles.title_wrapper}>
+              <p className={styles.title}>{product.title}</p>
+              <p className={styles.price}>${product.price}</p>
+            </div>
+            <p className={styles.description}>{product.description}</p>
           </div>
-          <p className={styles.description}>{product.description}</p>
         </div>
       </Link>
       <button
@@ -74,16 +76,15 @@ export default function Product({ product }: { product: Product }) {
           <div style={{ marginTop: 10 }}>
             <Link href="/cart">
               <button
-                className={styles.button}
-                style={{ marginBottom: 0, marginTop: 5, marginLeft: "0" }}
+                className={styles.button2}
               >
                 Cart
               </button>
             </Link>
             <button
               onClick={() => setModalVisible(false)}
-              className={styles.button}
-              style={{ marginBottom: 0, marginTop: 5, marginLeft: 5 }}
+              className={styles.button2}
+              style={{marginLeft:5}}
             >
               Close
             </button>
